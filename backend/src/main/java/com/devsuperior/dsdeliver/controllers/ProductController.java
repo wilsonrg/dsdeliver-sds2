6379d8c77283d99c02e56eef703e2730731c13d8
wilsonrg <wil.rgomes@gmail.com>
2021-01-09 21:14:12 -0300

    POST /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/controllers/ProductController.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/controllers/ProductController.java
@@ -8,18 +8,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devsuperior.dsdeliver.dto.OrderDTO;
-import com.devsuperior.dsdeliver.services.OrderService;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
+import com.devsuperior.dsdeliver.services.ProductService;
 
 @RestController
-@RequestMapping(value = "/orders")
+@RequestMapping(value = "/products")
 public class ProductController {
 	@Autowired
-	private OrderService service;
+	private ProductService service;
 	
 	@GetMapping
-	public ResponseEntity<List<OrderDTO>> findAll(){
-		List<OrderDTO> list = service.findAll();
+	public ResponseEntity<List<ProductDTO>> findAll(){
+		List<ProductDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
